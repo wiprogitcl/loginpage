62b18c25331f86f725453f01d8399d4af778bdf0
wiprogitcl <wiprocldemo01@outlook.com>
2025-05-22 14:32:19 +0530

    Update login.php

--- login.php
+++ login.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 
-$conn = new mysqli("localhost", "root", "", "login_demo");
+$conn = new mysqli("mydemosvraz.database.windows.net", "wiproadmin", "Server@1", "mydemodb");
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
